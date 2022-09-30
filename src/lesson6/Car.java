dd98dd56ba21c261defae3d0e4944c42d98932fc
s.skvortsov <sergei.skvortsov10@gmail.com>
2022-09-30 17:20:33 +0300

    added package lesson 6

---
+++ src/lesson6/Car.java
@@ -0,0 +1,20 @@
+package lesson6;
+
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+
+    }
+    private void startCommand() {
+
+    }
+    private void startFuelSystem() {
+
+    }
+}
